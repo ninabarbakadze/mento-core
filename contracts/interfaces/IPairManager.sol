941c853e533cfad7e7f8dff3ccdf64cd5b997934
Bayological <6872903+bayological@users.noreply.github.com>
2022-08-18 16:16:44 -0500

    feat: add mcMint interfaces

---
+++ contracts/interfaces/IPairManager.sol
@@ -0,0 +1,47 @@
+pragma solidity 0.8.16;
+
+/**
+ * @title Pair Manager Interface
+ * @notice The pair manager is responsible for managing the state of all Mento virtual pairs.
+ */
+interface IPairManager {
+    struct Pair {
+        address stableAsset;
+        address collateralAsset;
+        address mentoExchange;
+        uint256 stableBucket;
+        uint256 collateralBucket;
+        uint256 bucketUpdateFrequency;
+        uint256 lastBucketUpdate;
+        // FixidityLib.Fraction collateralBucketFraction; // TODO: Decide on fixed point math lib or continue using Fixidity 🫠
+        // FixidityLib.Fraction stableBucketMaxFraction;
+        // FixidityLib.Fraction spread;
+        uint256 minimumReports;
+        uint256 minSupplyForStableBucketCap;
+        bytes32 stableTokenRegistryId;
+    }
+
+    /**
+     * @notice Emitted when a new virtual pair has been created.
+     * @param stableAsset The address of the stable asset.
+     * @param collateralAsset The address of the collateral asset.
+     * @param mentoExchange The address of the Mento exchange.
+     */
+    event PairCreated(address indexed stableAsset, address indexed collateralAsset, address indexed mentoExchange);
+
+    /**
+     * @notice Emitted when a virtual pair has been destroyed.
+     * @param stableAsset The address of the stable asset.
+     * @param collateralAsset The address of the collateral asset.
+     * @param mentoExchange The address of the Mento exchange.
+     */
+    event PairDestroyed(address indexed stableAsset, address indexed collateralAsset, address indexed mentoExchange);
+
+    /**
+     * @notice Retrieves the pair with the specified pairId.
+     * @param pairId The id of the pair to be retrieved.
+     * @return found A boolean indicating whether a pair with the given pairId was found.
+     * @return pair The pair information.
+     */
+    function getPair(bytes32 pairId) external view returns (bool found, Pair memory pair);
+}
